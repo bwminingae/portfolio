44fe141dfa0915cd4b03cb62c37a60fa97e0c4e3
bwminingae <contact@bw-mining.ae>
2026-03-20 12:58:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -575,7 +575,7 @@ cards = [
                 en cours
                 <br>
                  <div style="
-                margin-top: 30px;
+                margin-top: 25px;
                 ">
                 Total actuel (cash + positions) :
                 <span style="font-weight:600;">
