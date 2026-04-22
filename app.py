dabd04c1d1c4ba8be4bdf4591df517008729edef
bwminingae <contact@bw-mining.ae>
2026-04-22 16:51:45 +0400

    Update app.py

--- app.py
+++ app.py
@@ -688,7 +688,7 @@ margin-bottom:12px;
 background:rgba(255,255,255,0.03);
 border:1px solid rgba(255,255,255,0.06);
 border-radius:14px;
-padding:14px 18px;
+padding:12px 18px;
 box-sizing:border-box;
 ">
 
